64221552a460dd74dc9633b74a5f4ab0abe9156e
tolaseadegbite <tolasekelvin@gmail.com>
2023-09-02 18:00:30 +0100

    Build user product page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,5 +6,10 @@ class ProductsController < ApplicationController
 
     def show
         @product ||= Product.find(params[:id])
+        @products = Product.all.take(3)
+        @related_products = Product.find(Product.pluck(:id).sample(3))
+        # count = Product.count
+        # random_offset = rand(3)
+        # random_product = Product.offset(random_offset).first
     end
 end
