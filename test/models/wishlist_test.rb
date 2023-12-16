8dc703ec3acbd094f520610cc8d4b5209a96f3f7
tolaseadegbite <tolasekelvin@gmail.com>
2023-12-16 14:08:08 +0100

    Wishlist model test

--- test/models/wishlist_test.rb
+++ test/models/wishlist_test.rb
@@ -22,7 +22,18 @@
 require "test_helper"
 
 class WishlistTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:tolase)
+    @product = products(:product1)
+    @wishlist = @user.wishlists.build(product: @product)
+  end
+
+  test "wishlist must be valid" do
+    @wishlist.valid?
+  end
+
+  test "user must be present" do
+    @wishlist.user = nil
+    assert_not @wishlist.valid?
+  end
 end
