f08ba972be66a8a22600aed0c52c8c6e5297c2f6
tolaseadegbite <tolasekelvin@gmail.com>
2023-12-17 16:03:32 +0100

    wishlist controller test

--- test/controllers/wishlists_controller_test.rb
+++ test/controllers/wishlists_controller_test.rb
@@ -1,7 +1,35 @@
 require "test_helper"
 
 class WishlistsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:tolase)
+    @product = products(:product1)
+    @base_title = 'Grills by Lashe'
+  end
+
+  test "should redirect index when not logged in" do
+    get my_wishlists_path
+    assert_redirected_to new_user_session_url
+    assert_not flash.empty?
+  end
+
+  test "should redirect create when now logged in" do
+    get product_path(@product)
+    assert_template 'products/show'
+    assert_no_difference 'Wishlist.count' do
+      post product_wishlists_path(@product), params: { wishlist: { product: @product } }
+    end
+    assert_redirected_to new_user_session_url
+    assert_not flash.empty?
+  end
+
+  test "should redirect destroy when now logged in" do
+    get product_path(@product)
+    assert_template 'products/show'
+    assert_no_difference 'Wishlist.count' do
+      delete product_wishlist_path(@product), params: { wishlist: { product: @product } }
+    end
+    assert_redirected_to new_user_session_url
+    assert_not flash.empty?
+  end
 end
