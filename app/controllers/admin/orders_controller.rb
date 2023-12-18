0fe4ed1a4b5d6eb3f53cf136e240f9bd7c67b99d
tolaseadegbite <tolasekelvin@gmail.com>
2023-12-18 01:26:10 +0100

    Admin orders simple search

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,7 @@
 class Admin::OrdersController < Admin::BaseController
 
   def index
-    @orders = UserProduct.all
+    @orders = UserProduct.order(id: :desc).where(["transaction_id LIKE ?","%#{params[:search]}%"])
   end
 
   def show
